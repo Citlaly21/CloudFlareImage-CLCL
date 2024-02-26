8d1a1554d010e35a10eae0e995e3ae4767fe6c98
Citlaly21 <cruzlisset87@gmail.com>
2024-02-25 22:21:25 -0600

    Versión final

--- cloudflare/CloudflareImageUploader.js
+++ cloudflare/CloudflareImageUploader.js
@@ -1,38 +1,37 @@
-// CloudflareImageUploader.js
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-const CloudflareImageUploader = () => {
-    const [image, setImage] = useState(null);
-    const [variants, setVariants] = useState({});
+const useCloudflareImageUploader = () => {
+    const [variants, setVariants] = useState([]);
 
-    const submitImage = async () => {
-        if (!image) {
-            console.log("No hay imagen seleccionada para subir.");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append("file", { uri: image, name: 'image.jpg', type: 'image/jpg' });
-
+    const submitImage = async (formData) => {
         try {
-            const response = await axios.post("https://api.cloudflare.com/client/v4/accounts/<ca9b43f77ee269734e8818fd05c17671>/images/v1",  
-            formData, {
-                headers: {
-                    "Authorization": "Bearer hDEkJ8ZmQ19KFpumZzrm0sk1N-O5ElIO-jq23ytT",
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            
+            const response = await axios.post(
+                "https://api.cloudflare.com/client/v4/accounts/ca9b43f77ee269734e8818fd05c17671/images/v1",
+                formData,
+                {
+                    headers: {
+                        "Authorization": "Bearer xZw5UJ8-sKv23-ruvrQKITPWi3QOHGo36NEGXTaP",
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            console.log("Response from Cloudflare API:", response.data); // Agregar esta línea
+
             const { result } = response.data;
-            const { variants } = result;
-            setVariants(variants);
+            if (result && result.variants) {
+                setVariants(result.variants);
+            } else {
+                console.error("La respuesta de la API no contiene el campo 'result' o 'variants'.");
+            }
         } catch (error) {
             console.error("Error al subir la imagen:", error);
         }
     };
 
-    return { image, setImage, variants, setVariants, submitImage };
+    return { variants, submitImage };
 };
 
-export default CloudflareImageUploader;
+
+export default useCloudflareImageUploader;
--- image.js
+++ image.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Text, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import CloudflareImageUploader from './cloudflare/CloudflareImageUploader';
+import useCloudflareImageUploader from './cloudflare/CloudflareImageUploader'; 
+
 
 export default function ImagePickerExample() {
-    const { variants, setVariants, image, setImage, submitImage } = CloudflareImageUploader();
+    const { submitImage, variants } = useCloudflareImageUploader();  
     const [isImageSelected, setIsImageSelected] = useState(false);
+    const [image, setImage] = useState(null);
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -15,47 +17,44 @@ export default function ImagePickerExample() {
             quality:  1,
         });
 
-        if (!result.cancelled) {
+        if (!result.cancelled && result.assets.length >  0 && result.assets[0].uri) {
             setImage(result.assets[0].uri);
             setIsImageSelected(true);
         }
     };
 
-    const takePhoto = async () => {
-        const { status } = await ImagePicker.requestCameraPermissionsAsync();
-
-        if (status !== 'granted') {
-            alert('Se necesitan los permisos para acceder a la camara!');
-            return;
+    const uploadImage = async () => {
+        if (isImageSelected) {
+            const formData = new FormData();
+            formData.append("file", { uri: image });
+            submitImage(formData);
         }
+    };
 
-        let result = await ImagePicker.launchCameraAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4,  3],
-            quality:  1,
-        });
-
-        if (!result.cancelled) {
-            setImage(result.uri);
+    useEffect(() => {
+        if (variants.length >  0) {
             setIsImageSelected(true);
         }
-    };
-
-    const uploadImage = () => {
-        if (isImageSelected) {
-            submitImage();
-        }
-    };
+    }, [variants]);
 
     return (
-        <View style={{ flex:  1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button title="Elige una imagen" onPress={pickImage} />
-            <Button title="Toma una foto" onPress={takePhoto} />
-            {isImageSelected && (
-                <Button title="Subir" onPress={uploadImage} />
-            )}
-            {image && <Image source={{ uri: image }} style={{ width:  200, height:  200 }} />}
-        </View>
+        <ScrollView contentContainerStyle={{ flexGrow:  1 }}>
+            <View style={{ flex:  1, alignItems: 'center', justifyContent: 'center', padding:  20 }}>
+                <Button title="Elige una imagen" onPress={pickImage} />
+                {isImageSelected && (
+                    <>
+                        <Button title="Subir" onPress={uploadImage} />
+                        <Image source={{ uri: image }} style={{ width:  200, height:  200, marginTop:  20 }} />
+                        {variants.map((variantUrl, index) => (
+                            <View key={index} style={{ alignItems: 'center', justifyContent: 'center', marginTop:  10 }}>
+                                <Text>Variante {index +  1}</Text>
+                                <Image source={{ uri: variantUrl }} style={{ width:  200, height:  200 }} />
+                                <Text>{variantUrl}</Text>
+                            </View>
+                        ))}
+                    </>
+                )}
+            </View>
+        </ScrollView>
     );
 }
